517d212f86728f7b2c4da16806982cc92499bee0
simo <simo@DESKTOP-22U71ER.localdomain>
2026-04-06 19:29:46 +0100

    beta

--- python_10/ex0/lambda_spells.py
+++ python_10/ex0/lambda_spells.py
@@ -1,28 +1,57 @@
 def artifact_sorter(artifacts: list[dict]) -> list[dict]:
-    return sorted(artifacts)
+    return sorted(artifacts, key=lambda x: x['power'], reverse=True)
 
 
 def power_filter(mages: list[dict], min_power: int) -> list[dict]:
-    pass
+    return list(filter(lambda x: x['power'] >= min_power, mages))
 
 
 def spell_transformer(spells: list[str]) -> list[str]:
-    pass
+    return list(map(lambda x: f"* {x} *", spells))
 
 
 def mage_stats(mages: list[dict]) -> dict:
-    pass
+    result = {
+        'max_power': max(mages, key=lambda x: x['power'])['power'],
+        'min_power': min(mages, key=lambda x: x['power'])['power'],
+        'avg_power': round(sum(map(lambda x: x['power'], mages)) / len(mages), 2)
+    }
+    return result
 
 
 def main() -> None:
     artifacts = [
-        {'name': 'Crystal Orb', 'power': 85, 'type': 'scrying'},
-        {'name': 'Fire Staff', 'power': 92, 'type': 'weapon'},
-        {'name': 'Healing Amulet', 'power': 45, 'type': 'trinket'},
-        {'name': 'Thunder Hammer', 'power': 100, 'type': 'weapon'}
+        {'name': 'Crystal Orb', 'power': 71, 'type': 'accessory'},
+        {'name': 'Shadow Blade', 'power': 88, 'type': 'weapon'},
+        {'name': 'Ice Wand', 'power': 93, 'type': 'focus'},
+        {'name': 'Shadow Blade', 'power': 111, 'type': 'accessory'}
     ]
 
+    mages = [
+        {'name': 'Jordan', 'power': 73, 'element': 'wind'},
+        {'name': 'Riley', 'power': 74, 'element': 'water'},
+        {'name': 'Morgan', 'power': 81, 'element': 'lightning'},
+        {'name': 'Phoenix', 'power': 51, 'element': 'earth'},
+        {'name': 'Kai', 'power': 69, 'element': 'light'}
+    ]
+
+    spells = ['shield', 'meteor', 'tornado', 'blizzard']
+
+    print()
+    print("Testing artifact sorter...")
     print(artifact_sorter(artifacts))
+
+    print()
+    print("Testing power filter...")
+    print(power_filter(mages, 70))
+
+    print()
+    print("Testing spell transformer...")
+    print(spell_transformer(spells))
+
+    print()
+    print("Testing mage stats...")
+    print(mage_stats(mages))
 
 
 if __name__ == '__main__':
